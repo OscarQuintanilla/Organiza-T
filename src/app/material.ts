7b80af3665828fc9de4ade2c1a31736313ba7ae0
Oscar Quintanilla <oscar.david.quintanilla@gmail.com>
2019-01-17 14:02:20 -0600

    +Se personalizó el tema  +Se avanzó en el formulario de registro de tareas

--- src/app/material.ts
+++ src/app/material.ts
@@ -5,21 +5,24 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatInputModule} from '@angular/material/input';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
 
 
 @NgModule({
     imports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatDividerModule, MatListModule, MatCardModule,
-        MatToolbarModule, MatSidenavModule, MatMenuModule, MatIconModule, MatRadioModule, MatSelectModule],
+        MatToolbarModule, MatSidenavModule, MatMenuModule, MatIconModule, MatRadioModule, MatSelectModule, 
+        MatDatepickerModule, MatNativeDateModule],
     exports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatDividerModule, MatListModule, MatCardModule,
         MatToolbarModule, MatSidenavModule, MatMenuModule, MatIconModule, MatRadioModule, MatSelectModule, MatFormFieldModule,
-        MatSlideToggleModule, ]
-}) 
+        MatSlideToggleModule, MatDatepickerModule, MatNativeDateModule,]
+})
 export class MaterialModule { }
